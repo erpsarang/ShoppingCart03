ae90438c4cf55718313634e21cb0d86f72619c8c
erpsarang <sangyeul.han@gmail.com>
2015-06-27 19:10:20 +0900

    3rd commit
    3rd video code sample

---
+++ WebContent/common.js
@@ -0,0 +1,18 @@
+jQuery.sap.declare("sap.demo.cart.common");
+
+sap.demo.cart.common = {
+		
+		method1: function() {
+			
+			alert('metho1 called');
+			
+		},
+		
+		addToCart: function() {
+			
+			alert('add to cart called');
+			
+			
+		}
+		
+}
